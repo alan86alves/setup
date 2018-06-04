ba531c96b50bf27f371bdc1a94c19796773dc9ed
Alan Alves <alan86alves@gmail.com>
2018-06-04 16:23:50 -0300

    Update install-docker.sh

--- manjaro/install-docker.sh
+++ manjaro/install-docker.sh
@@ -2,21 +2,10 @@
 # curl https://gist.githubusercontent.com/alan86alves/52257c7eba66beeffbb39bcf9658fd7a/raw/ea7a49e70419598baad7a52bd53db3e61b81a78c/install-docker.sh | bash
 
 # Remove older installations
-sudo pacman -Rs docker docker-engine docker.io
+sudo pacman -Rs docker
 
 # Installing Required Software
-sudo pacman -Syu apt-transport-https ca-certificates curl software-properties-common
-
-# Install certificates
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-
-# Setup repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# Install
-sudo apt-get update
-sudo apt-get install -y docker-ce apt-cache madison docker-ce
+sudo pacman -S docker
 
 # Creauser to user docker without sudo
 sudo groupadd docker
