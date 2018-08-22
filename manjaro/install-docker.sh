3ccd7d56b387990e4a588eecaeb487bc050443a1
Alan Alves <alan86alves@gmail.com>
2018-08-22 10:45:14 -0300

    Update install-docker.sh

--- manjaro/install-docker.sh
+++ manjaro/install-docker.sh
@@ -13,3 +13,15 @@ sudo systemctl start docker
 # Creauser to user docker without sudo
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+# Test the installation.
+docker -v
+
+# Run this command to download the latest version of Docker Compose:
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary:
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Test the installation.
+docker-compose --version
